77d9d52ed5919262829f020fb421b3e2d84cccec
Paulo Torres <pveg@live.com>
2022-08-24 12:13:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      BestCastFM
+    </h1>
+    </div>
+  );
+}
+
+export default App;
